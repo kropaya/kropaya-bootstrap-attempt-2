1370de5673639594ad1fb6fc72a839b545089bea
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-06-20 21:32:08 +1200

    Add more symbols, plus whitespace.

--- src/main/scala/net/flaviusb/kropaya/parser.scala
+++ src/main/scala/net/flaviusb/kropaya/parser.scala
@@ -6,8 +6,8 @@ object KropayaParser extends RegexParsers {
   override type Elem = Char
   override def skipWhitespace = false
   def variable: Parser[Variable] =
-    "\\A((([_+]+[_+:]*)?[a-zA-Z][a-zA-Z0-9_$!?%=-]*)|([~!@$^*_='`?★×÷≠⧺⧻§∘•·≢∧∨∪∩□◊⊃+-]+)|([\\\\])|…)(\\^[+-])?".r ^^ {
+    "\\A((([_+]+[_+:]*)?[a-zA-Z][a-zA-Z0-9_$!?%=-]*)|([~!<>@$^*_='`?★×÷≠⧺⧻§∘•·≢∧∨∪∩□◊◁▷⊃+-]+)|([\\\\])|…)(\\^[+-])?".r ^^ {
       x => Variable(x)
     }
-  def expression: Parser[Expression] = variable 
+  def expression: Parser[Expression] = variable
 }
