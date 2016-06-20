e88c506a328dbcf0ed1ee14438bff4a0702e1cf7
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-06-20 21:56:50 +1200

    Simple bracketed expression.

--- src/main/scala/net/flaviusb/kropaya/parser.scala
+++ src/main/scala/net/flaviusb/kropaya/parser.scala
@@ -2,6 +2,8 @@ package net.flaviusb.kropaya
 
 import scala.util.parsing.combinator._
 
+import scala.language.postfixOps
+
 object KropayaParser extends RegexParsers {
   override type Elem = Char
   override def skipWhitespace = false
@@ -9,5 +11,7 @@ object KropayaParser extends RegexParsers {
     "\\A((([_+]+[_+:]*)?[a-zA-Z][a-zA-Z0-9_$!?%=-]*)|([~!<>@$^*_='`?★×÷≠⧺⧻§∘•·≢∧∨∪∩□◊◁▷⊃+-]+)|([\\\\])|…)(\\^[+-])?".r ^^ {
       x => Variable(x)
     }
+  def ws: Parser[String] = "[ ]+".r ^^ { _ => "" }
+  def bracketed_expression: Parser[Expression] = "(" ~ ws ~ expression ~ ws ~ ")" ^^ { case "(" ~ _ ~ x ~ _ ~ ")" => x }
   def expression: Parser[Expression] = variable
 }
