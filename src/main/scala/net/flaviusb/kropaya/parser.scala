3acda085c7267e66290c6e67b85e3ed2ca1373aa
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-06-21 17:17:07 +1200

    Start parsing 'thing' s.

--- src/main/scala/net/flaviusb/kropaya/parser.scala
+++ src/main/scala/net/flaviusb/kropaya/parser.scala
@@ -7,15 +7,20 @@ import scala.language.postfixOps
 object KropayaParser extends RegexParsers {
   override type Elem = Char
   override def skipWhitespace = false
+  val name_pattern = "\\A((([_+]+[_+:]*)?[a-zA-Z][a-zA-Z0-9_$!?%=-]*)|([~!<>@$^*_='`?★×÷≠⧺⧻§∘•·≢∧∨∪∩□◊◁▷⊃+-]+)|([\\\\])|…)(\\^[+-])?".r;
   def variable: Parser[Variable] =
-    "\\A((([_+]+[_+:]*)?[a-zA-Z][a-zA-Z0-9_$!?%=-]*)|([~!<>@$^*_='`?★×÷≠⧺⧻§∘•·≢∧∨∪∩□◊◁▷⊃+-]+)|([\\\\])|…)(\\^[+-])?".r ^^ {
+     name_pattern ^^ {
       x => Variable(x)
     }
   def ws: Parser[String] = "[ ]+".r ^^ { _ => "" }
+  def nl: Parser[String] = "\r\n" ^^ { _ => "" }
   def bracketed_expression: Parser[Expression] = "(" ~ ws ~ expression ~ ws ~ ")" ^^ { case "(" ~ _ ~ x ~ _ ~ ")" => x }
   def application: Parser[Expression] = expression_part ~ (((ws ~ expression_part) ^^ {case _ ~ arg => arg })+) ^^ {
     case fn ~ args => Application(fn, args)
   }
   def expression_part: Parser[Expression] = bracketed_expression | variable
   def expression: Parser[Expression] = application | expression_part
+  def thing: Parser[Thing] = "%thing" ~ ws ~ name_pattern ~ nl ~ "%end" ^^ { 
+    case _ ~ _ ~ name ~ _ ~ _ => Thing(name, Array(), Array(), Array())
+  }
 }
